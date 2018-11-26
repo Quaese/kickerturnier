e291526d6049ba75a5a54af4b0ad17bd40db4bd1
Quaese <quaese@gmx.net>
2018-11-26 19:50:30 +0100

    Kickerturnier - Implementation
    * update participants
    * style lists

--- src/app/models/tournament.models.ts
+++ src/app/models/tournament.models.ts
@@ -1,41 +1,37 @@
 // Definiert die Strunktur eines Teams
 export interface Team {
-  name: string;
-  player1: string;
-  player2: string;
+    name: string;
+    player1: string;
+    player2: string;
 }
 
 // Definiert einen Zustand (wird in der reducer-index im "State" der Eigenschaft "tournament" zugewiesesn)
 export interface TournamentState {
-  teams: Team[];
-  teamNames: string[];
-  player: string[];
+    teams: Team[];
+    teamNames: string[];
+    player: string[];
 }
 
 // Vordefinierte Team Namen
 export const teamNames: string[] = [
-  'Terrifying Metal Wipeout',
-  'Outer Steel Squall',
-  'Streetwise Sweat Savages',
-  'The Pinewoods Ramblers',
-  'Watery Enchiladas',
-  'Streaking Liberty Hatters',
-  'The Chillers',
-  'Savage Thunder Pussies'
+    'Terrifying Metal Wipeout',
+    'Outer Steel Squall',
+    'Streetwise Sweat Savages',
+    'The Pinewoods Ramblers',
+    'Watery Enchiladas',
+    'Streaking Liberty Hatters',
+    'The Chillers',
+    'Savage Thunder Pussies'
 ];
 
 // Teilnehmer
 export const player: string[] = [
-  'Carina',
-  'Marcel',
-  'Marcus',
-  'Maik',
-  'Fouad',
-  'Peter',
-  'Björn',
-  'Alex',
-  'Friedrich',
-  'Lukas',
-  'Carsten',
-  'Norman'
+    'Carina',
+    'Marcus',
+    'Maik',
+    'Peter',
+    'Fouad-Steffen',
+    'Oleksandr',
+    'Friedrich',
+    'Lukas'
 ]
